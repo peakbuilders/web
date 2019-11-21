62d62e6b32788689ef0e7bce42c35ff616ddab26
Eric Gross <github@ericgross.com>
2019-11-21 17:30:34 -0500

    Add join spec

--- spec/system/account_spec.rb
+++ spec/system/account_spec.rb
@@ -5,16 +5,16 @@ require 'rails_helper'
 describe 'accounts' do
   include_context 'mock_data'
 
+  let(:mock_gateway) { double('gateway') }
+  let(:client_token) { OpenStruct.new(generate: 'your_client_token') }
+  let(:transaction) { double('transaction') }
+  let(:successful_result) { Braintree::SuccessfulResult.new(transaction: mock_transaction) }
+
   before do
-    @mock_gateway = double('gateway')
-    generator = OpenStruct.new(generate: 'your_client_token')
-    expect(@mock_gateway).to receive('client_token').and_return(generator).exactly(2).times
-
-    expect(Braintree::Gateway).to receive(:new).and_return(@mock_gateway).exactly(3).times
-
-    expect(@mock_gateway).to receive_message_chain('transaction.sale') {
-      Braintree::SuccessfulResult.new(transaction: mock_transaction)
-    }
+    expect(Braintree::Gateway).to receive(:new).and_return(mock_gateway).exactly(4).times
+    expect(mock_gateway).to receive('client_token').and_return(client_token).exactly(2).times
+    expect(mock_gateway).to receive('transaction').and_return(transaction).exactly(2).times
+    expect(transaction).to receive('sale').and_return(successful_result).exactly(2).times
   end
 
   it 'allow a user to join' do
@@ -22,5 +22,9 @@ describe 'accounts' do
     click_link 'Join'
     click_button 'Join now'
     expect(page).to have_css('.alert', text: '{"email"=>["can\'t be blank"]}')
+
+    fill_in 'account_email', with: 'test@test.com'
+    click_button 'Join now'
+    expect(page).to have_css('.heading', text: 'And we\'re glad glad glad that you\'ve arrived')
   end
 end
