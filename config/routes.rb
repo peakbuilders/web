a67ee656a7ec57b4e8a70e06df79b9439c069aa4
Eric Gross <github@ericgross.com>
2020-02-24 09:34:49 -0500

    Comment out accounts and payment

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@
 Rails.application.routes.draw do
   devise_for :accounts, skip: [:registrations]
 
-  resources :accounts, only: %i[new create]
-  resources :payments, only: %i[new create]
+  # resources :accounts, only: %i[new create]
+  # resources :payments, only: %i[new create]
   resources :members, only: %i[index show]
   resources :profiles, only: %i[edit update]
 
